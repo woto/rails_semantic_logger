ef18eb80d661e423931548860ca3a8cba64bd2e8
Ruslan Kornev <oganer@gmail.com>
2020-02-14 10:15:30 +0300

    Demo

--- lib/rails_semantic_logger/grape/log_subscriber.rb
+++ lib/rails_semantic_logger/grape/log_subscriber.rb
@@ -1,28 +1,31 @@
 module RailsSemanticLogger
   module Grape
     class LogSubscriber < ActiveSupport::LogSubscriber
-      def endpoint_render(event)
-        self.class.logger.info('a' => 'endpoint_render')
-      end
-
       def logger
         logger = SemanticLogger['Grape']
       end
 
+      def endpoint_render(event)
+        # self.class.logger.info('a' => 'endpoint_render')
+      end
+
       def endpoint_run_validators(event)
-        logger.info('a' => 'endpoint_run_validators')
+        # logger.info('a' => 'endpoint_run_validators')
+        logger.info(params: event.payload[:request].params,
+                    path: event.payload[:request].path,
+                    method: event.payload[:request].request_method)
       end
 
       def endpoint_run(event)
-        logger.info('a' => 'endpoint_run')
+        # logger.info('a' => 'endpoint_run')
       end
 
       def endpoint_run_filters(event)
-        logger.info('a' => 'endpoint_run_filters')
+        # logger.info('a' => 'endpoint_run_filters')
       end
 
       def format_response(event)
-        logger.info('a' => 'format_response')
+        # logger.debug('a' => 'format_response')
       end
     end
   end
--- lib/rails_semantic_logger/twirp/log_subscriber.rb
+++ lib/rails_semantic_logger/twirp/log_subscriber.rb
@@ -8,17 +8,17 @@ module RailsSemanticLogger
 
       def route_request(event)
         logger.info(:rpc_method => event.payload[:env][:rpc_method], :input => event.payload[:env][:input])
-        logger.info('a' => 'route_request')
+        # logger.info('a' => 'route_request')
       end
 
       def hook(event)
-        logger.info(:rpc_method => event.payload[:env][:rpc_method], :input => event.payload[:env][:input])
-        logger.info('a' => 'hook')
+        # logger.info(:rpc_method => event.payload[:env][:rpc_method], :input => event.payload[:env][:input])
+        # logger.info('a' => 'hook')
       end
 
       def handler(event)
-        logger.info(:rpc_method => event.payload[:env][:rpc_method], :input => event.payload[:env][:input])
-        logger.info('a' => 'handler')
+        # logger.info(:rpc_method => event.payload[:env][:rpc_method], :input => event.payload[:env][:input])
+        # logger.info('a' => 'handler')
       end
     end
   end
